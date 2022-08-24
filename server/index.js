5195aa8558f8ce237237000204b9c0badf453e08
Ligoh <ligohthealive@gmail.com>
2022-08-24 10:22:59 +0900

    auth, logout, react install

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ const port = 3000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { User } = require("./models/User");
+const {auth} = require('./middleware/auth');
 
 const config = require("./config/key")
+
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
@@ -25,7 +27,7 @@ app.get('/', (req, res) => {
   res.send('server request!')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
   //회원 가입할 때 필요 한 정보들을 client에서 가져오면 그 것들을 데이터 베이스에 넣어준다.
     
    const user = new User(req.body)
@@ -38,7 +40,7 @@ app.post('/register', (req, res) => {
    })
 })
 
-app.post('/login', (req,res)=> {
+app.post('/api/users/login', (req,res)=> {
     //요청된 이메일을 데이터베이스에서 있는지 찾기
     User.findOne({email: req.body.email}, (err, user)=>{
        if(!user){
@@ -66,9 +68,31 @@ app.post('/login', (req,res)=> {
          })
 
     })
-   
-
 })
+
+app.get('/api/users/auth', auth ,(req, res)=>{
+      //미들웨어를 통과하면 Authentication이 True
+      res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.roll === 0 ?false: true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+      })
+})
+
+app.get('/api/users/logout', auth, (req, res)=>{
+   User.findOneAndUpdate({_id: req.user._id}, {token: ''}, (err, user)=>{
+     if(err) return res.json({success: false, err});
+     return res.status(200).send({
+       success: true
+     })
+   })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- server/models/User.js
+++ server/models/User.js
@@ -55,23 +55,36 @@ userSchema.pre('save', function(next){
     
 })
 
-userSchema.methods.comparePassword = function(plainPassword, cbf){
+userSchema.methods.comparePassword = function(plainPassword, cb){
     bcrypt.compare(plainPassword, this.password, function(err, isMatch){
-        if(err) return cbf(err)
-        cbf(null, isMatch)
+        if(err) return cb(err)
+        cb(null, isMatch)
     })
 }
 
-userSchema.methods.genToken = function(cbf) {
+userSchema.methods.genToken = function(cb) {
     let user = this;
     //jsonwebtoken을 이요해서 토큰생성
     let token  = jwt.sign(user._id.toHexString(), 'secretToken')
     user.token = token
     user.save(function(err, user){
-        if(err) return cbf(err)
-        cbf(null, user)
+        if(err) return cb(err)
+        cb(null, user)
     })
     
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+     var user = this ;
+     //토큰을 복호화
+     jwt.verify(token, 'secretToken', function(err, decoded){
+        
+        //유저아이디를 이용해서 유저를 찾은 다음에 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+        user.findOne({"_id" : decoded, "token" : token}, function(err,user){
+            if(err) return cb(err);
+            cb(null, user)
+        })
+     })
 }
 
 const User = mongoose.model('User', userSchema)
