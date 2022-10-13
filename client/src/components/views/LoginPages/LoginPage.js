1634e4172654696494c1ae1560b7963c3909f00e
Ligoh <ligohthealive@gmail.com>
2022-10-13 10:53:35 +0900

    login page 동작함

--- client/src/components/views/LoginPages/LoginPage.js
+++ client/src/components/views/LoginPages/LoginPage.js
@@ -1,9 +1,15 @@
 import {React, useState} from 'react'
+import {useDispatch} from 'react-redux';
+import {loginUser} from '../../../_actions/user_action'
+import { useNavigate  } from 'react-router-dom';
 
-function LoginPage() {
+function LoginPage(props) {
+  const dispatch = useDispatch();
 
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
+
+  const navigate = useNavigate();
 
   const emailHandler = (event) =>{
     setEmail(event.currentTarget.value)
@@ -12,7 +18,25 @@ function LoginPage() {
     setPassword(event.currentTarget.value)
   }
   const submitHandler = (event) =>{
-    
+    event.preventDefault();
+    console.log('Email', Email)
+    console.log('Password', Password)
+
+    let body = {
+      email: Email,
+      password: Password
+    }
+
+    dispatch(loginUser(body))
+    .then(response=>{
+      if(response.payload.loginSuccess){
+        navigate('/')
+      }else{
+        alert('Error')
+      }
+    });
+   
+
   }
   return (
     <div style={{display: 'flex', justifyContent:'center' , alignItems: 'center' , width: '100%', height: '100vh'}}>
@@ -32,5 +56,6 @@ function LoginPage() {
     </div>
   )
 }
+
 
 export default LoginPage
